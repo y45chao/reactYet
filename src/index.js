105252772dc9b936b2698399ec4d917f0d2f1ee5
yangchao <y45chao@126.com>
2017-03-22 11:31:28 +0800

    东西放的太多乱掉了，当做经验了

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React,{Component}  from "react";
+import {render} from 'react-dom';
+
+class Main extends Component{
+	render(){
+		return (
+			<h1>react Yet</h1>
+			)
+	}
+} 
+
+render(
+	<Main/>,
+	document.body
+);
